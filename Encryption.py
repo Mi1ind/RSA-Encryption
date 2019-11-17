56cc141ae9a4220ba88b3ca8a32b6a0e07114c6c
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2019-11-17 18:37:58 -0500

    Encryption works

--- Encryption.py
+++ Encryption.py
@@ -1,18 +1,18 @@
 
 class encrypt():
-    publicKey = [5, 14]
 
     def strToNum(self, txt):
         num = []
-        let = 0
-
-        # for i in range(len(txt)):
-        #     num.append(my_dict[txt[i]])
-        # print(num)
         for i in range(len(txt)):
             num.append(ord(txt[i]))
         print(''.join(map(str, num)))
 
+    def maths(self, asciiTxt):
+        publicKey = [5, 14]
+        num = (asciiTxt**publicKey[0]) % publicKey[1]
+        print(num)
+
 
 a = encrypt()
-a.strToNum('Hi I am Milind')
+# a.strToNum('ab')
+a.maths(9798)
