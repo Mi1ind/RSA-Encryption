b4c4244287370ed872b760d3f4ea59156ed5628b
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2019-11-18 00:44:39 -0500

    Incorrect keys

--- Encryption.py
+++ Encryption.py
@@ -13,11 +13,12 @@ class Encrypt(object):
         print(''.join(map(str, num)))
 
     def maths(self, asciiTxt):
-        publicKey = [143, 7]
+        publicKey = [145906768007583323230186939349070635292401872375357164399581871019873438799005358938369571402670149802121818086292467422828157022922076746906543401224889672472407926969987100581290103199317858753663710862357656510507883714297115637342788911463535102712032765166518411726859837988672111837205085526346618740053,
+                     65537]  # [N,e] where N = p*q and e is encryption key
         nums = (asciiTxt**publicKey[1]) % publicKey[0]
         print(nums)
 
 
 newInstance = Encrypt()
-newInstance.strToNum('aa')
-newInstance.maths(2)
+# newInstance.strToNum("Hi my")
+newInstance.maths(7210532109121)
